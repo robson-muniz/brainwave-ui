76302472f0eb0fc5283383b8fe196da89804da20
robson-muniz <robson_muniz@yahoo.com>
2026-01-27 12:37:45 +0000

    feat(router): fetch data via TanStack Router loaders

--- src/routes/ideas/$ideaId/index.tsx
+++ src/routes/ideas/$ideaId/index.tsx
@@ -1,9 +1,23 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+const fetchIdeas = async ({ ideaId }: { ideaId: string }) => {
+  const res = await fetch(`http://localhost:8000/ideas/${ideaId}`)
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch idea')
+  }
+
+  return res.json()
+}
+
 export const Route = createFileRoute('/ideas/$ideaId/')({
   component: ideaDetalPage,
+  loader: async ({ params }) => {
+    return fetchIdeas({ ideaId: params.ideaId })
+  }
 })
 
 function ideaDetalPage() {
-  return <div>Hello "/ideas/$ideaId/"!</div>
+  const idea = Route.useLoaderData()
+  return <div>{idea.title}</div>
 }
